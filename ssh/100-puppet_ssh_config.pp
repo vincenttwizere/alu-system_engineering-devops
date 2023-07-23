77c5744d510e8aabdda358954f209d7f4dd2cfa8
vincenttwizere <v.twizere@alustudent.com>
2023-07-23 20:08:02 +0000

    Client configuration file (w/ Puppet)

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,4 @@
-lient configuration file with Puppet
+# Client configuration file with Puppet
 file_line {  'passw_no_autentication':
   path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no',
